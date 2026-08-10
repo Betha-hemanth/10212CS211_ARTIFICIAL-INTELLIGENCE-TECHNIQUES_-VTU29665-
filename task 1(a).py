f50a91733e22b665c350bf0718944e6e74bee22f
BETHA HEMANTH <hemanthbetha50@gmail.com>
2026-08-10 09:00:48 +0530

    commit

---
+++ task 1(a).py
@@ -0,0 +1,24 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+def bfs(start):
+    visited = []
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
+
+    print("BFS Traversal:", " ".join(visited))
+
+bfs('A')
